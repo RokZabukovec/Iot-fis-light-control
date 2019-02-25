7ca94f0f5c915e1b82d65fb305771d24dd78e527
rokzabukovec <kzabuk@gmail.com>
2019-02-25 17:50:55 +0000

    Added blink function for one of the diods

---
+++ Primer03.js
@@ -0,0 +1,58 @@
+var http = require("http");
+var firmata = require("firmata");
+
+var board = new firmata.Board("/dev/ttyACM0", function(){// ACM (Abstract Control Model)
+    board.pinMode(8, board.MODES.OUTPUT);                                // za serijsko komunikacijo z Arduinom (lahko je USB)
+    board.pinMode(13, board.MODES.OUTPUT); // Posamezni pin konfiguriramo, da deluje kot vhod ali izhod
+});
+
+http.createServer(function(req, res){ // http.createServer([requestListener])
+                                      // "requestListener" je funkcija, ki se avtomatsko doda
+                                      // k dogodku 'request'.
+    var deli = req.url.split("/"); // razdelimo url glede na znak "/"
+    var operator1 = parseInt(deli[1],10); // 10 osnova številskega sistema, decimalno (od 2 do 36)
+    var operator2 = parseInt(deli[2],10); // 10 osnova številskega sistema, decimalno (od 2 do 36) 
+        
+     if (operator1 == 0) {
+        console.log("Izključevanje LED1");
+        board.digitalWrite(13, board.LOW);
+    }
+
+    if (operator1 == 1) {
+        console.log("Vključevanje LED1");
+        board.digitalWrite(13, board.HIGH);
+    }
+    if (operator2 == 0) {
+        console.log("Izključevanje LED2");
+        board.digitalWrite(8, board.LOW);
+    }
+    if (operator2 == 1) {
+        console.log("Vključevanje LED2");
+        board.digitalWrite(8, board.HIGH);
+    }   
+    
+    var oneON = 0;
+    function blink(){
+        if (oneON == 0) {
+          board.digitalWrite(13, board.LOW);
+          oneON = 1;
+        }else{
+          board.digitalWrite(13, board.HIGH);
+          oneON = 0; 
+        }
+    }
+
+    if (operator1 == 3 && operator2 == 3) {
+        console.log("Blink LED1");
+        setInterval(function(){
+            blink();
+        }, 1000);
+        
+    }
+    
+    
+    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    res.write("Za test vpišite v brskalnikovo vrstico z naslovom: http://192.168.1.136:8080/1/1 <br>");
+    res.write("ali: http://192.168.1.136:8080/0/0<br>");
+    res.end("Vrednost operatorja1: " + operator1 + "<br>" + "Vrednost operatorja2: " + operator2);
+}).listen(8080, "192.168.1.204");
